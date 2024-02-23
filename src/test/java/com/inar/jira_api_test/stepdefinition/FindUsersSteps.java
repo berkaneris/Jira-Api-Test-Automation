167c54526961caff3b8fdc1ed4ea44621f829299
MFAthesecond <furkanaltuntasofficial@gmail.com>
2024-02-23 10:50:05 +0300

    initial process

--- src/test/java/com/inar/jira_api_test/stepdefinition/FindUsersSteps.java
+++ src/test/java/com/inar/jira_api_test/stepdefinition/FindUsersSteps.java
@@ -4,31 +4,58 @@ import com.inar.jira_api.utils.ConfigManager;
 import com.inar.jira_api_test.stepdefinition.hook.BaseSteps;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.When;
-import io.restassured.common.mapper.TypeRef;
+import io.cucumber.java.it.Ma;
+import io.cucumber.java.sl.In;
+import org.assertj.core.api.Assertions;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class FindUsersSteps extends BaseSteps {
+    String findUserEndpoint = ConfigManager.getProperty("find_user_url");
+    String parameter;
+    @When("the client sets the request body to find user with query {string}")
+    public void theClientSetsTheRequestBodyToFindUserWith(String parameter) {
+        response = request
+                .queryParam("query" , parameter)
+                .when()
+                .get(findUserEndpoint);
 
-    String findUsersEndpoint = ConfigManager.getProperty("find_user_url");
-
-    @When("the client sets the request body to find user with {string}")
-    public void theClientSetsTheRequestBodyToFindUserWith(String query) {
-        Map<String,String> queryParam=new HashMap<>();
-        queryParam.put("query",query);
-        request = request.queryParams(queryParam);
-    }
-
-    @When("the client sends a POST request to find user")
-    public void theClientSendsAPOSTRequestToFindUser() {
-response=request.contentType("application/json").when().get(findUsersEndpoint);
+        this.parameter = parameter;
     }
 
     @And("the response should contain users data")
     public void theResponseShouldContainUsersData() {
+        JSONArray userRes = new JSONArray(response.getBody().asString());
 
+        for (int i = 0; i < userRes.length(); i++) {
+            JSONObject user = userRes.getJSONObject(i);
+            Assertions.assertThat(((String)user.get("displayName")).contains(parameter)).isTrue();
+            Assertions.assertThat(((String)user.get("accountId"))).isNotEmpty();
+        }
     }
 
+    @And("the response should contain {string} users")
+    public void theResponseShouldContainUsers(String maxResult) {
+        JSONArray userRes = new JSONArray(response.getBody().asString());
+        Assertions.assertThat(userRes.length()).isEqualTo(Integer.parseInt(maxResult));
+    }
 
+    @When("the client sets the request body to find user with maxResult {string}")
+    public void theClientSetsTheRequestBodyToFindUserWithMaxResult(String maxResult) {
+        int max= Integer.parseInt(maxResult);
+        System.out.println(max+"        **********************");
+
+        Map<String, Object> queryParams=new HashMap<>();
+        queryParams.put("query","");
+        queryParams.put("maxResults",max);
+
+        response = request
+                .queryParams(queryParams)
+                .when()
+                .get(findUserEndpoint);
+    }
 }
