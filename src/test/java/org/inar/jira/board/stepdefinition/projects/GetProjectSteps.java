9eceb0662f65e66c55d525beacc82faa85c00457
MehmetNafiz <mehmetnafizavsar@gmail.com>
2024-02-23 23:51:39 +0300

    update

--- src/test/java/org/inar/jira/board/stepdefinition/projects/GetProjectSteps.java
+++ src/test/java/org/inar/jira/board/stepdefinition/projects/GetProjectSteps.java
@@ -5,21 +5,26 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.When;
 import org.assertj.core.api.SoftAssertions;
 import org.inar.jira.board.stepdefinition.hook.BaseSteps;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Map;
 
 import static org.assertj.core.api.BDDAssertions.then;
 
-
 public class GetProjectSteps extends BaseSteps {
+    private static final Logger logger = LogManager.getLogger(GetProjectSteps.class);
 
     @When("I get details of project whose ID is {string}")
     public void ıGetDetailsOfProjectWhoseIDIsIDOfProject(String projectID) {
+        logger.info("Getting details of project with ID: {}", projectID);
         response = request.pathParam("projectIdOrKey", projectID).get("/rest/api/3/project/{projectIdOrKey}");
+        logger.info("Received response: {}", response.asString());
     }
+
     @And("Given values should match with response")
     public void valuesForUserCreatedProjectAndSituationActivenessAndProjectNameAnd(DataTable dataTable) {
-        Map<String,String> parameters =dataTable.asMap(String.class, String.class);
+        Map<String,String> parameters = dataTable.asMap(String.class, String.class);
 
         SoftAssertions softAssertions = new SoftAssertions();
         softAssertions.assertThat(response.jsonPath().getString("lead.displayName")).isEqualTo(parameters.get("Display Name"));
@@ -27,10 +32,11 @@ public class GetProjectSteps extends BaseSteps {
         softAssertions.assertThat(response.jsonPath().getString("name")).isEqualTo(parameters.get("ProjectName"));
         softAssertions.assertThat(response.jsonPath().getString("projectTypeKey")).isEqualTo(parameters.get("Project type Key"));
         softAssertions.assertAll();
-   }
+    }
 
     @And("Project ID should matches with {string}")
     public void projectIDShouldMatchesWithIDOfProject(String projectID) {
+        logger.info("Verifying project ID matches with expected value: {}", projectID);
         then(projectID).isEqualTo(response.jsonPath().getString("key"));
     }
 }
--- src/test/java/org/inar/jira/board/stepdefinition/projects/GetRecentProjectsSteps.java
+++ src/test/java/org/inar/jira/board/stepdefinition/projects/GetRecentProjectsSteps.java
@@ -8,24 +8,31 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.assertj.core.api.SoftAssertions;
 import org.json.JSONArray;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 import java.util.Map;
 
 public class GetRecentProjectsSteps extends BaseSteps {
+    private static final Logger logger = LogManager.getLogger(GetRecentProjectsSteps.class);
+
     String getRecentProjectsEndpoint = ConfigManager.getProperty("get_recent_projects_url");
 
     String expandInformation;
 
     @When("user send a GET request the recent projects")
     public void userSendAGETRequestTheRecentProjects() {
+        logger.info("Sending a GET request to fetch recent projects");
         response = request
                 .when()
                 .get(getRecentProjectsEndpoint);
+        logger.info("Received response: {}", response.asString());
     }
 
     @And("the response should include the following project names")
     public void theResponseShouldIncludeTheFollowingProjectNames(DataTable dataTable) {
+        logger.info("Verifying response includes the following project names");
         List<Map<String, String>> projectsData = dataTable.asMaps(String.class, String.class);
         JSONArray projectsArray = new JSONArray(response.getBody().asString());
         SoftAssertions softAssertions = new SoftAssertions();
@@ -39,17 +46,19 @@ public class GetRecentProjectsSteps extends BaseSteps {
 
     @When("user send a GET request the recent projects with expanded information {string}")
     public void userSendAGETRequestTheRecentProjectsWithExpandedInformation(String expandList) {
+        logger.info("Sending a GET request to fetch recent projects with expanded information: {}", expandList);
         response = request
                 .queryParam("expand", expandList)
                 .when()
                 .get(getRecentProjectsEndpoint);
+        logger.info("Received response: {}", response.asString());
 
         expandInformation = expandList;
-
     }
 
     @Then("the response should include project details with expanded information")
     public void theResponseShouldIncludeProjectDetailsWithExpandedInformation() {
+        logger.info("Verifying response includes project details with expanded information");
         JSONArray projectsArray = new JSONArray(response.getBody().asString());
         SoftAssertions softAssertions = new SoftAssertions();
         String[] expandElements = expandInformation.split(",");
@@ -57,10 +66,8 @@ public class GetRecentProjectsSteps extends BaseSteps {
             for (int j = 0; j < expandElements.length; j++) {
                 softAssertions.assertThat(projectsArray.getJSONObject(i).getString("expand").contains(expandElements[j])).isTrue();
                 softAssertions.assertThat(projectsArray.getJSONObject(i).has(expandElements[j])).isTrue();
-
             }
         }
         softAssertions.assertAll();
-
     }
 }
